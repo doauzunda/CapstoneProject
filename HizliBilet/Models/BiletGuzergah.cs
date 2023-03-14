c46d4ee31f969c9157d38037f4ae91a85dbea7d7
Doa <dogauzundag@hotmail.com>
2023-03-15 02:29:42 +0300

    Update2.0

---
+++ HizliBilet/Models/BiletGuzergah.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Entities;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HizliBilet.UILayer.Models
+{
+    public class BiletGuzergah
+    {
+        public List<EntityLayer.Entities.Bilet> Bilets { get; set; }
+        public List<Guzergah> Guzergahs { get; set; }
+        public List<Sehir> Sehirs { get; set; }
+        public Guzergah YeniGuzergah { get; set; }
+        public string saat { get; set; }
+        public string tarih { get; set; }
+        public Bilet bilet { get; set; }
+
+    }
+}
--- HizliBilet/Startup.cs
+++ HizliBilet/Startup.cs
@@ -1,3 +1,7 @@
+using DataAccess.Concrete;
+using DataAccess.Abstract;
+using HizliBilet.BusinessLayer.Abstract;
+using HizliBilet.BusinessLayer.Concrete;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -8,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HizliBilet.DataAccess.Concrete;
 
 namespace HizliBilet
 {
@@ -23,7 +28,14 @@ namespace HizliBilet
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddScoped<IBiletRepositoryDal, EfCoreBiletRepository>();
+
+            services.AddScoped<IBiletService, BiletManager>();
+            services.AddScoped<IGuzergahRepositoryDal, EfCoreGuzergahRepository>();
+            services.AddScoped<IGuzergahService, GuzergahManager>();
+            services.AddScoped<ISehirRepositoryDal, EfCoreSehirRepository>();
+            services.AddScoped<ISehirService, SehirManager>();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,11 +43,12 @@ namespace HizliBilet
         {
             if (env.IsDevelopment())
             {
+                SeedDatabase.Seed();
                 app.UseDeveloperExceptionPage();
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -49,7 +62,9 @@ namespace HizliBilet
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
